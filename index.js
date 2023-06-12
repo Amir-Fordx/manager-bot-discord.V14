2d5bcc025b3c81190efc48380ab23ba0cd329e03
AmirFordx <106273190+Amir-Fordx@users.noreply.github.com>
2023-06-12 14:33:55 +0330

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const port = 3012
 app.get('/', (req, res) => {
-    res.send('Hello Parzival')
+    res.send('Hello')
 })
 
 app.listen(port, () => {
